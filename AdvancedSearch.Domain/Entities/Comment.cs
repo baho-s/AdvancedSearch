bea9164d26183004eadb4d82bf09fa8114d92a0f
Bahadır Sabancı <88037188+baho-s@users.noreply.github.com>
2026-05-02 20:23:40 +0300

    refactor(domain): encapsulate Product and Comment entities

--- AdvancedSearch.Domain/Entities/Comment.cs
+++ AdvancedSearch.Domain/Entities/Comment.cs
@@ -9,6 +9,23 @@ namespace ShopSage.Domain.Entities
 {
     public class Comment:BaseEntity
     {
+        public string Content { get; private set; }
 
+        public int ProductId { get; private set; }
+        public Product Product { get; private set; }
+        public int CustomerId { get; private set; }
+        public Customer Customer { get; private set; }
+
+        protected Comment()
+        {
+
+        }
+
+        public Comment(string content, int customerId, int productId)
+        {
+            Content = content;
+            CustomerId = customerId;
+            ProductId = productId;
+        }
     }
 }
--- AdvancedSearch.Domain/Entities/Product.cs
+++ AdvancedSearch.Domain/Entities/Product.cs
@@ -8,8 +8,35 @@ using System.Threading.Tasks;
 
 namespace ShopSage.Domain.Entities
 {
-    public class Product:BaseEntity, IAggregateRoot
+    public class Product : BaseEntity, IAggregateRoot
     {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Information { get; private set; }
+        public string Features { get; private set; }
+        public string Description { get; private set; }
+
+        private readonly List<ProductCategory> _productCategories = new();
+        public IReadOnlyCollection<ProductCategory> ProductCategories => _productCategories.AsReadOnly();
+
+        private readonly List<Comment> _comments = new();
+        public IReadOnlyCollection<Comment> Comments => _comments.AsReadOnly();
+
+        protected Product()
+        {
+
+        }
+
+        public Product(string name, decimal price, int stock, string information, string features, string description)
+        {
+            Name = name;
+            Price = price;
+            Stock = stock;
+            Information = information;
+            Features = features;
+            Description = description;
+        }
 
     }
 }
