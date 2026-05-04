59619602ba7826e8a98e6d1600c7b230e36ade99
Bahadır Sabancı <88037188+baho-s@users.noreply.github.com>
2026-05-04 15:25:58 +0300

    feat(infrastructure): implement Unit of Work for centralized repository management
    - Integrated repository properties into UnitOfWork for unified access.
    - Implemented SaveChangesAsync to enable atomic database updates in a single transaction.
    - Centralized AppDbContext management within the Unit of Work to ensure data integrity across the AdvancedSearch project.

--- AdvancedSearch.Domain/Interfaces/UnitOfWork/IUnitOfWork.cs
+++ AdvancedSearch.Domain/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AdvancedSearch.Domain.Interfaces.Repositories;
+using AdvancedSearchDomain.Interfaces.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,12 @@ namespace AdvancedSearchDomain.Interfaces.UnitOfWork
 {
     public interface IUnitOfWork
     {
+        IOrderRepository Orders { get; }
+        IProductRepository Products { get; }
+        ICustomerRepository Customers { get; }
+        ICategoryRepository Categories { get; }
 
+        Task<int> SaveChangesAsync();
     }
 }
 
---
+++ AdvancedSearch.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using AdvancedSearch.Domain.Interfaces.Repositories;
+using AdvancedSearch.Infrastructure.Context;
+using AdvancedSearch.Infrastructure.Repositories;
+using AdvancedSearchDomain.Interfaces.Repositories;
+using AdvancedSearchDomain.Interfaces.UnitOfWork;
+
+namespace AdvancedSearch.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public IOrderRepository Orders { get; private set; }
+        public IProductRepository Products { get; private set; }
+        public ICustomerRepository Customers { get; private set; }
+        public ICategoryRepository Categories { get; private set; }
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Orders = new OrderRepository(_context);
+            Products = new ProductRepository(_context);
+            Customers = new CustomerRepository(_context);
+            Categories = new CategoryRepository(_context);
+        }
+
+
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();            
+        }
+    }
+}
