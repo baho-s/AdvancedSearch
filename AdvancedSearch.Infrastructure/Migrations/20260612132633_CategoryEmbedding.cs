c9205098a949ee049c5392af028ac556fec685d7
Bahadır Sabancı <88037188+baho-s@users.noreply.github.com>
2026-06-12 16:28:05 +0300

    db(migration): add migration for CategoryEmbedding field

---
+++ AdvancedSearch.Infrastructure/Migrations/20260612132633_CategoryEmbedding.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Pgvector;
+
+#nullable disable
+
+namespace AdvancedSearch.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryEmbedding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Vector>(
+                name: "Embedding",
+                table: "Categories",
+                type: "vector(768)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Embedding",
+                table: "Categories");
+        }
+    }
+}
